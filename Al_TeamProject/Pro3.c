4fbe0c4fef2c3cb56041d2f4f3a6f10db8707b98
JuneYoungs <asplannedl@gmail.com>
2015-12-18 22:29:23 +0900

    주석 삭제 적용

--- Al_TeamProject/Pro3.c
+++ Al_TeamProject/Pro3.c
@@ -32,8 +32,6 @@ void feedBack();
 int stonePos();
 int findRoom();
 
-// 메인 ==============================================================
-// =================================================================
 void main() {
 	int i = 0, k = 0, select = 0;
 
@@ -155,10 +153,8 @@ void updateStone() {
 				if (g_stone[i][k] == 0) continue; 
 				if (g_stone[i][k] != g_recentPlayer) continue; 
 				if (g_stone[i][k] != 0) stonePos(g_stone[i][k], i, k, roomData, 0); 
-
-				//isRoom = findRoom(roomData); // 그곳이 집인지 판단
-				if (findRoom(roomData) == ROOM) { // 집인경우
-					for (del = 0; del<49; del++) { // 해당 리스트에 있는 돌들을 삭제
+				if (findRoom(roomData) == ROOM) {
+					for (del = 0; del<49; del++) {
 						if (roomData[del] == -1) continue;
 						data = g_stone[roomData[del] / 10][roomData[del] % 10];
 						switch (data) {
@@ -208,7 +204,6 @@ void makePlatform() {
 	printf("\n");
 
 	for (i = 0; i<platformHeight / 2; i++) {
-	//for (i = platformHeight / 2-1; i>=0; i--) {
 		printf(" %d├", 6 - i);
 		for (j = 0; j<platformHeight / 2; j++) {
 			printf("─");
@@ -242,34 +237,21 @@ void makePlatform() {
 
 
 int stonePos(int p_stoneNum, int x, int y, int *roomData, int checkRoot) {
-
-	// p_stoneNum : 돌색(1 or 2)
-	// x, y : 좌표
-	// checkRoot : 재귀 이용했으므로 갔던길을 되돌아 오지 않도록 하는 변수.
-	//    4 
-	//  1   3  으로 지정하여 처리함
-	//    2
-	// *location : 갔던길을 저장하는 변수
-
 	roomData[g_stonPos++] = x * 10 + y;
 
 	if (x - 1 >= 0 && checkRoot != 1) {
 		if (g_stone[x - 1][y] == p_stoneNum) stonePos(g_stone[x - 1][y], x - 1, y, roomData, 3);
 	}
-
 	if (y + 1 < 7 && checkRoot != 2) {
 		if (g_stone[x][y + 1] == p_stoneNum) stonePos(g_stone[x][y + 1], x, y + 1, roomData, 4);
 	}
-
 	if (x + 1 < 7 && checkRoot != 3) {
 		if (g_stone[x + 1][y] == p_stoneNum) stonePos(g_stone[x + 1][y], x + 1, y, roomData, 1);
 	}
-
 	if (y - 1 >= 0 && checkRoot != 4) {
 		if (g_stone[x][y - 1] == p_stoneNum) stonePos(g_stone[x][y - 1], x, y - 1, roomData, 2);
 	}
 	return 0;
-
 }
 
 
@@ -285,10 +267,6 @@ int findRoom(int *p_roomPos) {
 		playerA = g_stone[x][y];
 
 		playerB = (playerA == 1) ? 2 : 1;
-		//if (myType == 1) yourType = 2;
-		//if (myType == 2) yourType = 1;
-
-		// 4방향중 0(=빈칸)이 존재한다면 그것은 집이 아니다.
 		if ((g_stone[x - 1][y] == 0) && (x - 1 >= 0)) return NOROOM;
 		if ((g_stone[x + 1][y] == 0) && (x + 1 < 5)) return NOROOM;
 		if ((g_stone[x][y - 1] == 0) && (y - 1 >= 0)) return NOROOM;
@@ -301,33 +279,30 @@ void aiPlayer() {
 	int isFar = NOFAR;
 	int blackCnt = 0;
 	int maxBlackInfo = 100;
-	// ㄴ 125 : 2위치까지 검은색이 5개 있었다는 의미. 
-	//    9칸의 정사각형 바둑판 기준으로, 가장 왼쪽위가 1번째자리를 시작으로 반시계반향으로 돌면서 숫자가 커짐. 최대치 8
 	long seed = (long)time(NULL);
 	int ranX = -1, ranY = -1;
 	int i = 0;
 
-	int recentX = (g_save[g_saveNum - 1] / 100) % 10; // 1052 : x, y, type(0:빈칸,1:흑,2:백) 순서 1000은 더미
+	int recentX = (g_save[g_saveNum - 1] / 100) % 10; 
 	int recentY = (g_save[g_saveNum - 1] / 10) % 10;
 	int befroeRecentX = -1, befroeRecentY = -1;
 
 	g_recentPlayer = AI;
 	Sleep(2000);
-	// 3개 이상의 수가 놓여 졌을 때
 	if (g_saveNum - 3 >= 0) {
 		befroeRecentX = (g_save[g_saveNum - 3] / 100) % 10;
 		befroeRecentY = (g_save[g_saveNum - 3] / 10) % 10;
 	}
 
-	if (g_saveNum != 1) { // 동 떨어진덴다가 둘 경우의 사전 설정
-		if (recentX - 1 != befroeRecentX || recentX + 1 != befroeRecentX || recentY - 1 != befroeRecentY || recentY + 1 != befroeRecentY) { // 사람이 두 돌을 인접하게 두지 않았다면
+	if (g_saveNum != 1) { 
+		if (recentX - 1 != befroeRecentX || recentX + 1 != befroeRecentX || recentY - 1 != befroeRecentY || recentY + 1 != befroeRecentY) { 
 			recentX = befroeRecentX;
 			recentY = befroeRecentY;
 			isFar = FAR;
 		}
 	}
 
-	if (g_saveNum == 1 || isFar == FAR) { // 맨 처음이거나 사람이 동떨어진데다 둘 경우의 처리
+	if (g_saveNum == 1 || isFar == FAR) { 
 		if (recentX - 1 >= 0) {
 			if (g_stone[recentX - 1][recentY] == 0) {
 				g_stone[recentX - 1][recentY] = 2;
@@ -355,8 +330,7 @@ void aiPlayer() {
 		}
 	}
 
-	if (g_saveNum != 1 && isFar == NOFAR) { // 이전의 둔 곳과 비슷한 곳에 둔 경우의 처리 
-		// 최근의 컴퓨터가 둔 돌 기준으로, 사람이 둔 돌이 많은 쪽 옆에다가 돌을 두도록 한다. 
+	if (g_saveNum != 1 && isFar == NOFAR) { 
 		if (g_saveNum - 2 >= 0) {
 			befroeRecentY = (g_save[g_saveNum - 2] / 10) % 10;
 			befroeRecentX = (g_save[g_saveNum - 2] / 100) % 10;
@@ -416,7 +390,6 @@ void aiPlayer() {
 					maxBlackInfo = (maxBlackInfo / 100) * 100 + 80 + blackCnt;
 			}
 			else blackCnt = 0;
-			// 해당돌 왼쪽부터 시작하여 아래쪽으로 한바퀴 돌면서 체크함
 
 			switch ((maxBlackInfo / 10) % 10) {
 			case 0:
@@ -453,14 +426,11 @@ void aiPlayer() {
 				g_stone[befroeRecentX - 1][befroeRecentY - 1] = 2;
 				g_save[g_saveNum++] = 1000 + (befroeRecentX - 1) * 100 + (befroeRecentY - 1) * 10 + 2;
 				return;
-
 			}
 		}
 	}
 
-	// 이도저도 아닌 경우, 랜덤한, 빈칸의, 새로운 위치에 돌을 둔다.
 	srand(seed);
-
 	for (i = 0; i<49; i++) {
 		ranX = rand() % 5;
 		ranY = rand() % 5;
